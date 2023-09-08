20ad179039f368cbc3fd02cd18c397a365bcf052
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-09-08 22:32:33 +0200

    some examples

---
+++ examples/client.rs
@@ -0,0 +1,32 @@
+use std::net::{IpAddr, Ipv4Addr};
+
+use bevy::{pbr::wireframe::Wireframe, prelude::*, render::primitives::Aabb};
+use bevy_sync::{ClientPlugin, SyncComponent, SyncPlugin};
+
+fn main() {
+    let ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let port = 4000;
+
+    let mut client = App::new();
+    client.add_plugins(DefaultPlugins);
+    client.add_plugins(bevy_editor_pls::EditorPlugin::default());
+    client.add_plugins(SyncPlugin);
+    client.add_plugins(ClientPlugin { ip, port });
+
+    client.sync_component::<Aabb>();
+    client.sync_component::<Visibility>();
+    client.sync_component::<Transform>();
+    client.sync_component::<Wireframe>();
+    client.sync_component::<Handle<StandardMaterial>>();
+    client.sync_materials(true);
+
+    client.add_systems(Startup, load_world);
+    client.run();
+}
+
+fn load_world(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
---
+++ examples/host.rs
@@ -0,0 +1,66 @@
+use std::net::{IpAddr, Ipv4Addr};
+
+use bevy::{
+    pbr::wireframe::{Wireframe, WireframeConfig},
+    prelude::*,
+    render::primitives::Aabb,
+};
+use bevy_sync::{ServerPlugin, SyncComponent, SyncMark, SyncPlugin};
+
+fn main() {
+    let ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let port = 4000;
+    let mut host = App::new();
+    host.add_plugins(DefaultPlugins);
+    host.add_plugins(bevy_editor_pls::EditorPlugin::default());
+    host.add_plugins(SyncPlugin);
+    host.add_plugins(ServerPlugin { ip, port });
+
+    host.sync_component::<Aabb>();
+    host.sync_component::<Visibility>();
+    host.sync_component::<Transform>();
+    host.sync_component::<Wireframe>();
+    host.sync_component::<Handle<StandardMaterial>>();
+    host.sync_materials(true);
+
+    host.add_systems(Startup, load_world);
+    host.run();
+}
+
+fn load_world(
+    mut commands: Commands,
+    mut wireframe_config: ResMut<WireframeConfig>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    wireframe_config.global = false;
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..default()
+        },
+        SyncMark,
+    ));
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        },
+        Wireframe,
+        SyncMark,
+    ));
+    commands.spawn((
+        PointLightBundle {
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..default()
+        },
+        SyncMark,
+    ));
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
