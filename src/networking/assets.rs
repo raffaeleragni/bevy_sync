788e858f6000b05b7b05d948288426f1896f78db
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-12-20 20:33:08 +0100

    new minor

--- src/networking/assets.rs
+++ src/networking/assets.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 
-pub(crate) fn setup_client(app: &mut App, ip: std::net::IpAddr, web_port: u16) {
+pub(crate) fn setup_client(_: &mut App, _: std::net::IpAddr, _: u16) {
 }
 
-pub(crate) fn setup_server(app: &mut App, ip: std::net::IpAddr, web_port: u16) {
+pub(crate) fn setup_server(_: &mut App, _: std::net::IpAddr, _: u16) {
 }
