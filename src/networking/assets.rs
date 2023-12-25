59a2c2e8fdd88d7135f025730d4a9301e9d2fef7
Raffaele Ragni <raffaele.ragni@gmail.com>
2023-12-25 15:01:40 +0100

    setup for http

--- src/networking/assets.rs
+++ src/networking/assets.rs
@@ -1,27 +1,111 @@
-use std::net::{ToSocketAddrs, SocketAddr};
+use std::{
+    net::{SocketAddr, ToSocketAddrs},
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        Arc,
+    },
+};
 
-use bevy::prelude::*;
+use crate::mesh_serde::{bin_to_mesh, mesh_to_bin};
+use bevy::{prelude::*, utils::HashMap};
+use std::io::Read;
 use threadpool::ThreadPool;
-use tiny_http::Server;
+use tiny_http::{Request, Response, Server};
+
+const MAX_BYTES: u64 = 100_000_000;
 
 #[derive(Resource)]
-struct SyncAssetTransfer {
-    pool: ThreadPool,
-    server: Server,
+pub(crate) struct SyncAssetTransfer {
+    server_pool: ThreadPool,
+    download_pool: ThreadPool,
+    mesh_tx: Sender<Mesh>,
+    meshes: Arc<HashMap<String, Mesh>>,
+}
+
+pub(crate) enum Type {
+    Mesh,
 }
 
 impl SyncAssetTransfer {
-    fn new<A: ToSocketAddrs>(bind: A, thread_count: usize) -> Self {
-        let pool = ThreadPool::new(thread_count);
+    fn new<A: ToSocketAddrs>(bind: A) -> Self {
+        let server_pool = ThreadPool::new(2);
+        let download_pool = ThreadPool::new(127);
+        let meshes = Arc::new(HashMap::<String, Mesh>::new());
+
+        let (server_tx, server_rx) = channel::<Request>();
+        let (mesh_tx, mesh_rx) = channel::<Mesh>();
         let server = Server::http(bind).unwrap();
-        Self { pool, server }
+        let result = Self {
+            server_pool,
+            download_pool,
+            mesh_tx,
+            meshes,
+        };
+        result.server_pool.execute(move || {
+            for request in server.incoming_requests() {
+                server_tx.send(request).unwrap_or(());
+            }
+        });
+        let meshes = result.meshes.clone();
+        result
+            .server_pool
+            .execute(|| Self::respond(server_rx, meshes));
+
+        result
+    }
+
+    pub(crate) fn queue(&self, asset_type: Type, url: String) {
+        let tx = self.mesh_tx.clone();
+        self.download_pool.execute(move || {
+            if let Ok(response) = ureq::get(url.as_str()).call() {
+                let len = response
+                    .header("Content-Length")
+                    .and_then(|s| s.parse::<usize>().ok())
+                    .unwrap();
+                let mut bytes: Vec<u8> = Vec::with_capacity(len);
+                if response
+                    .into_reader()
+                    .take(MAX_BYTES)
+                    .read_to_end(&mut bytes)
+                    .is_ok()
+                {
+                    match asset_type {
+                        Type::Mesh => {
+                            let mesh = bin_to_mesh(bytes.as_slice());
+                            tx.send(mesh).unwrap_or(());
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    fn respond(rx: Receiver<Request>, meshes: Arc<HashMap<String, Mesh>>) {
+        for request in rx.iter() {
+            let url = request.url();
+            let Some(id) = url.strip_prefix("/mesh/") else {
+                continue;
+            };
+
+            let Some(mesh) = meshes.get(id) else {
+                request
+                    .respond(Response::from_string("").with_status_code(404))
+                    .unwrap_or(());
+                continue;
+            };
+            request
+                .respond(Response::from_data(mesh_to_bin(mesh)))
+                .unwrap_or(());
+        }
     }
 }
 
 pub(crate) fn setup(app: &mut App, addr: std::net::IpAddr, port: u16) {
     let thread_count = 1;
-    debug!("Initializing asset sync on {:?}:{}, parallel: {}", addr, port, thread_count);
-    let http_transfer = SyncAssetTransfer::new(SocketAddr::new(addr, port), thread_count);
+    debug!(
+        "Initializing asset sync on {:?}:{}, parallel: {}",
+        addr, port, thread_count
+    );
+    let http_transfer = SyncAssetTransfer::new(SocketAddr::new(addr, port));
     app.insert_resource(http_transfer);
 }
-
