2ec6a5b6504ab86bbf1ba3021a92ebe1e0f7d879
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-01-29 09:12:42 +0100

    fix index variant

--- src/networking/assets/mesh_serde.rs
+++ src/networking/assets/mesh_serde.rs
@@ -17,7 +17,8 @@ struct MeshData {
     colors: Option<Vec<[f32; 4]>>,
     joint_weights: Option<Vec<[f32; 4]>>,
     joint_indices: Option<Vec<[u16; 4]>>,
-    indices: Option<Vec<u32>>,
+    indices32: Option<Vec<u32>>,
+    indices16: Option<Vec<u16>>,
     morph_targets: Option<AssetImage>, //TODO serialize Image type
     morph_target_names: Option<Vec<String>>,
 }
@@ -71,7 +72,13 @@ pub(crate) fn mesh_to_bin(mesh: &Mesh) -> Vec<u8> {
         None
     };
 
-    let indices = if let Some(Indices::U32(t)) = mesh.indices() {
+    let indices32 = if let Some(Indices::U32(t)) = mesh.indices() {
+        Some(t.clone())
+    } else {
+        None
+    };
+
+    let indices16 = if let Some(Indices::U16(t)) = mesh.indices() {
         Some(t.clone())
     } else {
         None
@@ -97,7 +104,8 @@ pub(crate) fn mesh_to_bin(mesh: &Mesh) -> Vec<u8> {
         colors,
         joint_weights,
         joint_indices,
-        indices,
+        indices32,
+        indices16,
         morph_targets,
         morph_target_names,
     };
@@ -153,8 +161,11 @@ pub(crate) fn bin_to_mesh(binary: &[u8]) -> Mesh {
         mesh.insert_attribute(Mesh::ATTRIBUTE_JOINT_INDEX, Uint16x4(joint_indices));
     }
 
-    if let Some(indices) = data.indices {
+    if let Some(indices) = data.indices32 {
         mesh.set_indices(Some(Indices::U32(indices)));
+    }
+    if let Some(indices) = data.indices16 {
+        mesh.set_indices(Some(Indices::U16(indices)));
     }
 
     //if let Some(morph_targets) = data.morph_targets {
@@ -244,6 +255,77 @@ mod test {
     }
 
     #[test]
+    fn mesh_to_bin_to_mesh_idx16_compare() {
+        let mesh = sample_mesh_idx16();
+
+        let binary = mesh_to_bin(&mesh);
+        let mesh2 = bin_to_mesh(&binary[..]);
+
+        assert_eq!(mesh.primitive_topology(), mesh2.primitive_topology());
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_POSITION)
+                .unwrap()
+                .get_bytes(),
+            mesh2
+                .attribute(Mesh::ATTRIBUTE_POSITION)
+                .unwrap()
+                .get_bytes()
+        );
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_NORMAL).unwrap().get_bytes(),
+            mesh2.attribute(Mesh::ATTRIBUTE_NORMAL).unwrap().get_bytes()
+        );
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_UV_0).unwrap().get_bytes(),
+            mesh2.attribute(Mesh::ATTRIBUTE_UV_0).unwrap().get_bytes()
+        );
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_UV_1).unwrap().get_bytes(),
+            mesh2.attribute(Mesh::ATTRIBUTE_UV_1).unwrap().get_bytes()
+        );
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_TANGENT).unwrap().get_bytes(),
+            mesh2
+                .attribute(Mesh::ATTRIBUTE_TANGENT)
+                .unwrap()
+                .get_bytes()
+        );
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_COLOR).unwrap().get_bytes(),
+            mesh2.attribute(Mesh::ATTRIBUTE_COLOR).unwrap().get_bytes()
+        );
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_JOINT_WEIGHT)
+                .unwrap()
+                .get_bytes(),
+            mesh2
+                .attribute(Mesh::ATTRIBUTE_JOINT_WEIGHT)
+                .unwrap()
+                .get_bytes()
+        );
+        assert_eq!(
+            mesh.attribute(Mesh::ATTRIBUTE_JOINT_INDEX)
+                .unwrap()
+                .get_bytes(),
+            mesh2
+                .attribute(Mesh::ATTRIBUTE_JOINT_INDEX)
+                .unwrap()
+                .get_bytes()
+        );
+        let Indices::U16(v1) = mesh.indices().unwrap() else {
+            panic!("bad indices type")
+        };
+        let Indices::U16(v2) = mesh2.indices().unwrap() else {
+            panic!("bad indices type")
+        };
+        assert_eq!(
+            mesh.morph_target_names(),
+            mesh2.morph_target_names()
+        );
+         assert_eq!(v1, v2);
+    }
+
+    #[test]
     fn mesh_to_bin_to_mesh_compare_no_tangents() {
         let mesh = sample_mesh_no_tangents();
 
@@ -300,6 +382,27 @@ mod test {
             Uint16x4(vec![[1u16, 2, 3, 4]; 4]),
         );
         mesh.set_indices(Some(Indices::U32(vec![0, 2, 1])));
+        mesh.set_morph_target_names(vec!["name1".into(), "name2".into()]);
+        mesh
+    }
+
+    fn sample_mesh_idx16() -> Mesh {
+        let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+        mesh.insert_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            vec![[0., 0., 0.], [1., 2., 1.], [2., 0., 0.]],
+        );
+        mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, vec![[0., 1., 0.]; 3]);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![[0., 0.]; 3]);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_UV_1, vec![[1., 1.]; 3]);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_TANGENT, vec![[0., 1., 0., 0.]; 3]);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, vec![[0., 1., 0., 0.]; 4]);
+        mesh.insert_attribute(Mesh::ATTRIBUTE_JOINT_WEIGHT, vec![[0., 1., 0., 0.]; 4]);
+        mesh.insert_attribute(
+            Mesh::ATTRIBUTE_JOINT_INDEX,
+            Uint16x4(vec![[1u16, 2, 3, 4]; 4]),
+        );
+        mesh.set_indices(Some(Indices::U16(vec![0, 2, 1])));
         mesh.set_morph_target_names(vec!["name1".into(), "name2".into()]);
         mesh
     }
