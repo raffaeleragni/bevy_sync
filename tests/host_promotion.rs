00150b30d92bf2f4b5df8c6e51cd054af107fd90
Raffaele Ragni <raffaele.ragni@gmail.com>
2024-06-07 07:20:11 +0200

    check data sync after promotion

--- tests/host_promotion.rs
+++ tests/host_promotion.rs
@@ -1,8 +1,10 @@
-use bevy::app::App;
+use bevy::{app::App, ecs::entity::Entity};
 use bevy_renet::renet::{transport::NetcodeServerTransport, RenetServer};
-use bevy_sync::{PromoteToHostEvent, SyncConnectionParameters};
+use bevy_sync::{PromoteToHostEvent, SyncConnectionParameters, SyncMark};
 use serial_test::serial;
-use setup::{TestEnv, TestRun};
+use setup::{MySynched, TestEnv, TestRun};
+
+use crate::assert::get_first_entity_component;
 
 mod assert;
 mod setup;
@@ -14,6 +16,8 @@ fn test_host_promotion_with_one_client() {
         2,
         TestRun::no_pre_setup,
         |env| {
+            let e_id = setup_and_check_sync(env);
+
             // need this because the tests run on the same machine and promotion won't advance between
             // occupied server releasing connection on same port and client becoming server on same port
             alter_connection_port(env);
@@ -21,9 +25,47 @@ fn test_host_promotion_with_one_client() {
             send_promotion_event(env);
             env.update(10);
             assert_one_client_is_host(env);
+
+            env.server
+                .world
+                .entity_mut(e_id)
+                .get_mut::<MySynched>()
+                .unwrap()
+                .value = 7;
         },
-        |_, _, _| {},
+        |env, _, _| {
+            let comp = get_first_entity_component::<MySynched>(&mut env.clients[0]).unwrap();
+            assert_eq!(comp.value, 7);
+            let comp = get_first_entity_component::<MySynched>(&mut env.clients[1]).unwrap();
+            assert_eq!(comp.value, 7);
+        },
     );
+}
+
+fn setup_and_check_sync(env: &mut TestEnv) -> Entity {
+    env.setup_registration::<MySynched>();
+    let e_id = env.server.world.spawn(SyncMark {}).id();
+    env.update(4);
+    let mut e = env.server.world.entity_mut(e_id);
+    e.insert(MySynched { value: 1 });
+    env.update(4);
+    let comp = get_first_entity_component::<MySynched>(&mut env.clients[0]).unwrap();
+    assert_eq!(comp.value, 1);
+    let comp = get_first_entity_component::<MySynched>(&mut env.clients[1]).unwrap();
+    assert_eq!(comp.value, 1);
+    env.server
+        .world
+        .entity_mut(e_id)
+        .get_mut::<MySynched>()
+        .unwrap()
+        .value = 2;
+    env.update(4);
+    let comp = get_first_entity_component::<MySynched>(&mut env.clients[0]).unwrap();
+    assert_eq!(comp.value, 2);
+    let comp = get_first_entity_component::<MySynched>(&mut env.clients[1]).unwrap();
+    assert_eq!(comp.value, 2);
+
+    e_id
 }
 
 fn alter_connection_port(env: &mut TestEnv) {
